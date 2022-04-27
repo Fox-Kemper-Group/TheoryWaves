fe9ab9415f55e6af9d79f020eb1650ec5ecb0fe3
Paul Hall <paul_hall@brown.edu>
2022-04-27 15:56:14 -0400

    Changed indexing for U10 in HS

--- model/src/w3iogomd.F90
+++ model/src/w3iogomd.F90
@@ -2033,7 +2033,8 @@
 #endif
 !PSH TheoryWaves begin
 !            HS (JSEA) = 4. * SQRT ( ET(JSEA) ) ! original formulation
-            HS(JSEA) = U10(JSEA)
+!            HS(JSEA) = U10(JSEA)
+            HS(JSEA) = U10(ISEA)
 !            HS(JSEA) = 0.0246 * U10(JSEA)**2
 !PSH TheoryWaves end
 #ifdef W3_O9
